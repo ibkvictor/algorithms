420f7a0e66ca4bc095ca5b8a2f394e360fbd0c42
Victor Ezekiel <eze99victor@gmail.com>
2022-05-31 05:17:34 +0100

    dp problems solved

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -1,6 +1,16 @@
+#memoized solution = table[(i, j)]
+#subproblem = L(i,j) for all possible i and j
+#relate = L(i, j) = 1 or 0 + max(L(n, m)) for n and m in range (0, 1)
+#original = L(0,0)
+#time = O(n*n)
+#space = O(n*n)
+
 import math
 def solution(word1, word2):
+    table = {}
     def recur(i, j):
+        if (i, j) in table:
+            return table[(i,j)]
         v = math.inf
         if i < len(word1) and j < len(word2):
             if word1[i] != word2[j]:
@@ -18,7 +28,9 @@ def solution(word1, word2):
             v = len(word2) - i + 1
         else:
             v = 0
+        table[(i,j)] = v
         return v
+    print(table)
     return recur(0, 0)
 
-print(solution("", "ros"))
+print(solution("horse", "ros"))
